8888c0c758fadc6761195e9ad0f4d62de9d4306b
Tyhoo Wu <cnwutianhao@163.com>
2022-04-30 11:59:21 +0800

    Optimize data pulling by coroutine.

--- app/src/main/java/com/tyhoo/nba/viewmodel/SplashViewModel.kt
+++ app/src/main/java/com/tyhoo/nba/viewmodel/SplashViewModel.kt
@@ -1,13 +1,18 @@
 package com.tyhoo.nba.viewmodel
 
 import android.content.Context
+import android.util.Log
 import android.view.View
 import androidx.lifecycle.*
 import androidx.navigation.findNavController
 import com.tyhoo.nba.R
+import com.tyhoo.nba.base.TAG
 import com.tyhoo.nba.data.SplashRepository
 import com.tyhoo.nba.ui.splash.SplashFragmentDirections
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,15 +21,27 @@ class SplashViewModel @Inject constructor(
     private val repository: SplashRepository
 ) : ViewModel() {
 
-    suspend fun requestData(context: Context, owner: LifecycleOwner, view: View?) {
-        playersObserver(context, owner, view)
+    fun requestData(context: Context, owner: LifecycleOwner, view: View?) {
+        viewModelScope.launch {
+            val results = listOf(
+                async { playersObserver(context, owner) },
+                async { teamsObserver(context, owner) }
+            )
+            results.awaitAll()
+
+            delay(1000)
+            val direction = SplashFragmentDirections.actionSplashFragmentToHomeFragment()
+            view?.findNavController()?.navigate(direction)
+        }
     }
 
     private suspend fun playersObserver(
-        context: Context, owner: LifecycleOwner, view: View?
+        context: Context, owner: LifecycleOwner
     ) = Observer<Boolean> { result ->
         if (result == true) {
-            viewModelScope.launch { teamsObserver(context, owner, view) }
+            Log.d(TAG, "球员数据请求成功")
+        } else {
+            Log.d(TAG, "球员数据请求失败")
         }
     }.apply {
         _loadingContent.value = context.getString(R.string.splash_load_players)
@@ -32,11 +49,12 @@ class SplashViewModel @Inject constructor(
     }
 
     private suspend fun teamsObserver(
-        context: Context, owner: LifecycleOwner, view: View?
+        context: Context, owner: LifecycleOwner
     ) = Observer<Boolean> { result ->
         if (result == true) {
-            val direction = SplashFragmentDirections.actionSplashFragmentToHomeFragment()
-            view?.findNavController()?.navigate(direction)
+            Log.d(TAG, "球队数据请求成功")
+        } else {
+            Log.d(TAG, "球队数据请求失败")
         }
     }.apply {
         _loadingContent.value = context.getString(R.string.splash_load_teams)
